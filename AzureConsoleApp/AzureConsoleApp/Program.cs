f49878468d8578108f854a33d311ff93fc37b20f
sesubra <sesubra@gmail.com>
2017-05-16 19:23:48 -0400

    Initial Console App and a .NET Core APP
    Basic applicationa

---
+++ AzureConsoleApp/AzureConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SenthilPillai/SenthilPillai/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SenthilPillai.Startup))]
+namespace SenthilPillai
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
